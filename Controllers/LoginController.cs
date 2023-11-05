b1a0439bb8b73ac7b173fd8a88fa89239795c2af
HaiBang1010 <108066718+HaiBang1010@users.noreply.github.com>
2023-11-05 05:46:36 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,53 @@
+﻿using ChatChit.Models;
+using ChatChit.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChatChit.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private readonly ILoginService _loginService;
+        public LoginController(ILoginService loginService)
+        {
+            _loginService = loginService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login([FromBody] LoginModel login)
+        {
+            IActionResult response = Unauthorized();
+            if (login.email != null && login.password != null && login.email != "" && login.password != "")
+            {
+                var user = await _loginService.CheckInformation(login.email, login.password);
+                if (user != null)
+                {
+                    var tokenString = _loginService.GenerateJSONWebToken(user);
+                    response = Ok(new { token = tokenString });
+                    return response;
+                }
+                else
+                {
+                    var result = new
+                    {
+                        auth = false,
+                        message = "Wrong username/password!",
+                    };
+                    return NotFound(result);
+                }
+            }
+            else
+            {
+                var result = new
+                {
+                    auth = false,
+                    message = "Empty email/password"
+                };
+                return NotFound(result);
+            }
+        }
+    }
+}
---
+++ Services/LoginService.cs
@@ -0,0 +1,58 @@
+﻿using ChatChit.Data;
+using ChatChit.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using BC = BCrypt.Net.BCrypt;
+
+namespace ChatChit.Services
+{
+    public interface ILoginService
+    {
+        public Task<UserModel> CheckInformation(string email, string password);
+        public string GenerateJSONWebToken(UserModel user);
+    }
+
+    public class LoginService : ILoginService
+    {
+        private readonly ChatChitContex _context;
+        private readonly IConfiguration _config;
+
+        public LoginService(ChatChitContex context, IConfiguration config)
+        {
+            _context = context;
+            _config = config;
+        }
+
+        public async Task<UserModel> CheckInformation(string email, string password)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.email == email);
+            if (user == null)
+            {
+                return null;
+            }
+            if (BC.Verify(password, user.password))
+            {
+                return user;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public string GenerateJSONWebToken(UserModel user)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+            var token = new JwtSecurityToken(_config["Jwt:Issuer"],
+              _config["Jwt:Issuer"],
+              null,
+              expires: DateTime.Now.AddMinutes(120),
+              signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
